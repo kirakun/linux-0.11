e276d9594513d67e0e4dd2357d6f09c6fa85087c
kirakun <766605440@qq.com>
2020-11-01 22:40:00 +0800

    补充head.s的注释

--- boot/head.s
+++ boot/head.s
@@ -9,8 +9,10 @@
  *
  * NOTE!!! Startup happens at absolute address 0x00000000, which is also where
  * the page directory will exist. The startup code will be overwritten by
- * the page directory.
+ * the page directory.  
  */
+ ; * 注意!!! 32 位启动代码是从绝对地址0x00000000 开始的，这里也同样
+ ; * 是页目录将存在的地方，因此这里的启动代码将被页目录覆盖掉。
 .text
 .globl idt,gdt,pg_dir,tmp_floppy_area
 pg_dir:
@@ -20,17 +22,17 @@ startup_32:
 	mov %ax,%ds
 	mov %ax,%es
 	mov %ax,%fs
-	mov %ax,%gs
-	lss stack_start,%esp
-	call setup_idt
-	call setup_gdt
-	movl $0x10,%eax		# reload all the segment registers
+	mov %ax,%gs   # ds=es=fs=gs=0x10 段选择子指向gdt表中index=2的段选择符 也就是内核数据段
+	lss stack_start,%esp   #stack_start定义在 kernel/sched.c文件中 这里是设置内核栈指针 
+	call setup_idt  #调用设置中断描述符表子程序
+	call setup_gdt  #调用设置全局描述符表子程序。重置 gdt
+	movl $0x10,%eax		# reload all the segment registers 因为gdt变了 所以要重新设置段选择器 其实就是数据段
 	mov %ax,%ds		# after changing gdt. CS was already
 	mov %ax,%es		# reloaded in 'setup_gdt'
 	mov %ax,%fs
 	mov %ax,%gs
-	lss stack_start,%esp
-	xorl %eax,%eax
+	lss stack_start,%esp  #这条命令相当于设置 ss=0x10 esp=&user_stack [PAGE_SIZE>>2]
+	xorl %eax,%eax  # eax=0
 1:	incl %eax		# check that A20 really IS enabled
 	movl %eax,0x000000	# loop forever if it isn't
 	cmpl %eax,0x100000
@@ -78,17 +80,17 @@ check_x87:
  *  written by the page tables.
  */
 setup_idt:
-	lea ignore_int,%edx
+	lea ignore_int,%edx   # ignore_int是中断处理函数
 	movl $0x00080000,%eax
 	movw %dx,%ax		/* selector = 0x0008 = cs */
-	movw $0x8E00,%dx	/* interrupt gate - dpl=0, present */
+	movw $0x8E00,%dx	/* interrupt gate - dpl=0, present */  10001110 00000000
 
-	lea idt,%edi
-	mov $256,%ecx
+	lea idt,%edi       # idt的地址放在edi
+	mov $256,%ecx     # 循环256次 构建256个idt表项
 rp_sidt:
-	movl %eax,(%edi)
-	movl %edx,4(%edi)
-	addl $8,%edi
+	movl %eax,(%edi)   # selector=0x0008 中断处理函数指向ignore_int函数
+	movl %edx,4(%edi)   #中断属性
+	addl $8,%edi     # edi 指向下一个idt描述符位置 一个描述符8byte
 	dec %ecx
 	jne rp_sidt
 	lidt idt_descr
@@ -201,31 +203,37 @@ setup_paging:
 	movl $1024*5,%ecx		/* 5 pages - pg_dir+4 page tables */
 	xorl %eax,%eax
 	xorl %edi,%edi			/* pg_dir is at 0x000 */
-	cld;rep;stosl
+	/*cld设置edi或同esi为递增方向，rep做(%ecx)次重复操作，stosl表示edi每次增加4,这条语句达到按4字节清空前5*1024*4字节地址空间的目的
+	 * 这行代码是将0x00000~0x05000 之间 5*4kb=20kb的内存空间清零 用来存放page_dir和4个page_table
+	 * 当前最大寻址空间是16M 最多4096个page 4个page_table正好可以全部囊括
+	 */
+	cld;rep;stosl    
 	movl $pg0+7,pg_dir		/* set present bit/user r/w */
 	movl $pg1+7,pg_dir+4		/*  --------- " " --------- */
 	movl $pg2+7,pg_dir+8		/*  --------- " " --------- */
 	movl $pg3+7,pg_dir+12		/*  --------- " " --------- */
-	movl $pg3+4092,%edi
-	movl $0xfff007,%eax		/*  16Mb - 4096 + 7 (r/w user,p) */
+	movl $pg3+4092,%edi       # edi指向第4个page_table的最后一个页表项 一个页表项4byte 然后每次递减4
+	movl $0xfff007,%eax		/*  16Mb - 4096 + 7 (r/w user,p) 最后一个page的地址+属性 */
 	std
 1:	stosl			/* fill pages backwards - more efficient :-) */
-	subl $0x1000,%eax
+	subl $0x1000,%eax  # 每次减少一个page
 	jge 1b
 	xorl %eax,%eax		/* pg_dir is at 0x0000 */
 	movl %eax,%cr3		/* cr3 - page directory start */
-	movl %cr0,%eax
+	movl %cr0,%eax      
 	orl $0x80000000,%eax
-	movl %eax,%cr0		/* set paging (PG) bit */
+	movl %eax,%cr0		/* set paging (PG) bit */# cr3分页标识开启
 	ret			/* this also flushes prefetch-queue */
 
 .align 2
 .word 0
+  /* 下面两行是lidt 指令的6 字节操作数：长度，基址。*/
 idt_descr:
 	.word 256*8-1		# idt contains 256 entries
 	.long idt
 .align 2
 .word 0
+# 下面两行是lgdt 指令的6 字节操作数：长度，基址。
 gdt_descr:
 	.word 256*8-1		# so does gdt (not that that's any
 	.long gdt		# magic number, but it works for me :^)
